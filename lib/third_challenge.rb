09f727bf47d2a8455b3e252a049c0b5e7b139c25
Emily R <emilyroulund@gmail.com>
2019-04-25 03:56:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,15 +18,15 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => '54'
+        :name => "Lady Capulet",
+        :age => '51'
         },
       :heroine => {
-        :name => "Romeo",
+        :name => "Juliet",
         :age => "15",
         :status => "alive"
         },
